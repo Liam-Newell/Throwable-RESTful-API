8ba6bb9ca94231300dbe314cf744fecb70cfc642
Liam Newell <liam-newell@hotmail.com>
2018-01-29 07:34:18 -0500

    working phonenumber request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+// Require `PhoneNumberFormat`.
+var PNF = require('google-libphonenumber').PhoneNumberFormat;
+// Get an instance of `PhoneNumberUtil`.
+var phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* Lib phonenumber request*/
+router.get('/:string', function(req, res, next){
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+  try{
+      var phoneNumber = phoneUtil.parse(req.params.string, 'US');
+      res.end(phoneUtil.format(phoneNumber, PNF.INTERNATIONAL));
+  }
+  catch (a){
+      var error = "could not resolve " + req.params.string + " into phone number\n"
+      + "error: " + a;
+      res.end(error);
+    }
+
 });
 
 module.exports = router;
