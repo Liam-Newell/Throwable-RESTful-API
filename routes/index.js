308f2c780c967708f25725cf36cdd11ad2ba700c
Liam Newell <liam-newell@hotmail.com>
2018-01-29 08:18:44 -0500

    returns json object

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 // Require `PhoneNumberFormat`.
 var PNF = require('google-libphonenumber').PhoneNumberFormat;
 // Get an instance of `PhoneNumberUtil`.
@@ -12,7 +13,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* Lib phonenumber request*/
-router.get('/:string', function(req, res, next){
+router.get('/test/:string', function(req, res, next){
 
   try{
       var phoneNumber = phoneUtil.parse(req.params.string, 'US');
@@ -27,5 +28,27 @@ router.get('/:string', function(req, res, next){
     }
 
 });
+router.get('/file', function(req, res, next) {
+    res.render('file');
+});
+router.post('/file', function(req, res, next) {
+    var file = req.body.file;
+    var number;
+    fs.readFile(file, function (err, data) {
+        if (err) throw err;
+        number = data;
+    });
+    try{
+        var phoneNumber = phoneUtil.parse(number, 'US');
+        res.json(phoneUtil.format(phoneNumber, PNF.INTERNATIONAL))
+        //res.end(phoneUtil.format(phoneNumber, PNF.INTERNATIONAL));
+    }
+    catch (a){
+        var error = "could not resolve " + req.params.string + " into phone number\n"
+            + a;
+        res.end(error);
 
+    }
+
+});
 module.exports = router;
